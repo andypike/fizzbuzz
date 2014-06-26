e8930689a429af67d9d1faf6b006bd9f82fac0cf
Andy Pike <andy@andypike.com>
2014-06-26 22:56:13 +0100

    Pass spec: Populates form with range (9 specs 0 failures)

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -1,19 +1,13 @@
 class NumbersController < ApplicationController
   def index
-    @numbers = FizzBuzz.new.build_list(min..max)
+    @min = params_with_defaults[:numbers][:min].to_i
+    @max = params_with_defaults[:numbers][:max].to_i
+    @numbers = FizzBuzz.new.build_list(@min..@max)
   end
 
   private
 
   def params_with_defaults
-    params.reverse_merge!(:numbers => { :min => 1, :max => 10 })
-  end
-
-  def min
-    params_with_defaults[:numbers][:min].to_i
-  end
-
-  def max
-    params_with_defaults[:numbers][:max].to_i
+    params.reverse_merge(:numbers => { :min => 1, :max => 10 })
   end
 end
