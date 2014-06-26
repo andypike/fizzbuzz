1428da90106ce2dedbf0b5f46ad67ab6c03b5a24
Andy Pike <andy@andypike.com>
2014-06-26 23:40:35 +0100

    Pass specs: Validation specs passing (15 specs 0 failures)

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -1,6 +1,11 @@
 class NumbersController < ApplicationController
   def index
     @range = FizzBuzzRange.new(params[:fizz_buzz_range] || {})
-    @numbers = FizzBuzz.new.build_list(@range.range)
+
+    if @range.valid?
+      @numbers = FizzBuzz.new.build_list(@range.range)
+    else
+      @numbers = []
+    end
   end
 end
--- app/models/fizz_buzz_range.rb
+++ app/models/fizz_buzz_range.rb
@@ -3,12 +3,16 @@ class FizzBuzzRange
 
   attr_accessor :min, :max
 
+  validates :min, :max,
+    :presence => true,
+    :numericality => true
+
   def initialize(params)
-    @min = params.fetch(:min, 1).to_i
-    @max = params.fetch(:max, 10).to_i
+    @min = params.fetch(:min, "1")
+    @max = params.fetch(:max, "10")
   end
 
   def range
-    min..max
+    min.to_i..max.to_i
   end
 end
