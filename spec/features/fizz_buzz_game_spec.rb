325744f2e9b97946bd3f20bbd50512e0da84fcd9
Andy Pike <andy@andypike.com>
2014-06-26 21:49:43 +0100

    New spec: Shows numbers for given range (2 specs 1 failure)

--- spec/features/fizz_buzz_game_spec.rb
+++ spec/features/fizz_buzz_game_spec.rb
@@ -5,4 +5,18 @@ describe "FizzBuzz game" do
     visit root_path
     expect(page).to have_content(/fizzbuzz/i)
   end
+
+  context "valid input" do
+    it "shows FizzBuzz numbers between a given range" do
+      visit root_path
+
+      fill_in "Min", :with => "1"
+      fill_in "Max", :with => "15"
+      click_on "Generate"
+
+      expect(page).to have_content(
+        "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz"
+      )
+    end
+  end
 end
