b53f5fb3760f4eea357d9cb65d03f6f3a56a6791
Andy Pike <andy@andypike.com>
2014-06-26 23:00:28 +0100

    New spec: Show validation errors (10 specs 1 failure)

--- spec/features/fizz_buzz_game_spec.rb
+++ spec/features/fizz_buzz_game_spec.rb
@@ -1,21 +1,22 @@
 require "rails_helper"
 
 describe "FizzBuzz game" do
-  it "shows the homepage" do
+  before do
     visit root_path
+  end
+
+  it "shows the homepage" do
     expect(page).to have_content(/fizzbuzz/i)
   end
 
   context "with no input" do
     it "defaults to show numbers from 1 to 10" do
-      visit root_path
       expect(page).to have_content(
         "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz"
       )
     end
 
     it "populates the form with current values" do
-      visit root_path
       expect(find_field("Min").value).to eq("1")
       expect(find_field("Max").value).to eq("10")
     end
@@ -23,8 +24,6 @@ describe "FizzBuzz game" do
 
   context "valid input" do
     it "shows FizzBuzz numbers between a given range" do
-      visit root_path
-
       fill_in "Min", :with => "1"
       fill_in "Max", :with => "15"
       click_on "Generate"
@@ -32,6 +31,15 @@ describe "FizzBuzz game" do
       expect(page).to have_content(
         "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz"
       )
+    end
+  end
+
+  context "invalid input" do
+    it "shows errors" do
+      fill_in "Min", :with => ""
+      click_on "Generate"
+
+      expect(page).to have_content("can't be blank")
     end
   end
 end
