e4f4438d81c038b79b8a0eb2a17249ca878ac855
Andy Pike <andy@andypike.com>
2014-06-26 22:22:34 +0100

    Pass spec: Returns ['FizzBuzz'] for 15..15 (7 specs 2 failures)

--- app/models/fizz_buzz.rb
+++ app/models/fizz_buzz.rb
@@ -2,7 +2,9 @@ class FizzBuzz
   def build_list(range)
     [].tap do |list|
       range.each do |n|
-        if n % 3 == 0
+        if n % 3 == 0 && n % 5 == 0
+          list << "FizzBuzz"
+        elsif n % 3 == 0
           list << "Fizz"
         elsif n % 5 == 0
           list << "Buzz"
