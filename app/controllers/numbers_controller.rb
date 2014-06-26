2d82a30d8448c7384265ea7059c72390530a74b4
Andy Pike <andy@andypike.com>
2014-06-26 21:58:35 +0100

    Passed spec: Displays fake number list (2 spec 0 failures)

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -1,4 +1,5 @@
 class NumbersController < ApplicationController
   def index
+    @numbers = %w(1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "numbers#index"
+
+  post "/" => "numbers#index"
 end
